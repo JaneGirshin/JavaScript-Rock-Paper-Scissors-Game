48958c12ae5fcff478161238cd673cccc8fbc8a8
JaneGirshin <Jane@girsh.in>
2020-05-20 16:35:30 +0300

    Setting up hands

--- script.js
+++ script.js
@@ -1,90 +1,77 @@
-class Game {
-    constructor(userPoints, computerPoints, userScore, computerScore, actionText){
-        this.userPoints = userPoints;
-        this.computerPoints = computerPoints;
-        this.userScore = userScore;
-        this.computerScore = computerScore;
-        this.actionText = actionText;
-        this.clear();
-    }
-
-    clear(){
-        userPoints = 0;
-        computerPoints = 0;
-        // this.userScore.innerText = 0;
-        // this.computerScore.innerText = 0;
-        // this.actionText.innerText = 0;
-    }
-
-    RandomChoice(){
-        let choices = ['rock', 'paper', 'scissors'];
-        let randomNumber = Math.floor(Math.random()*3);
-        return choices[randomNumber];
-    }
-
-    userPlays(userChoice){
-        let computerChoice = this.RandomChoice();
-        switch(userChoice+computerChoice){
-            case 'rockrock':
-            case 'paperpaper':
-            case 'scissorsscissors':
-                // this.userTie();
-                break;
-            case 'rockscissors':
-            case 'paperrock':
-            case 'scissorspaper':
-                // this.userWin();
-                // updateScore(user)
-                userPoints++;
-                console.log(userPoints);
-                this.updateScore();
-                break;
-            case 'rockpaper':
-            case 'paperscissors':
-            case 'scissorsrock':
-                // this.userLose();
-                break;
-        }
-    }
-
-    // userTie(){
-    //     console.log('its a tie');
-    // }
-
-    // userWin(){
-    //     console.log('its a win');
-    // }
-
-    // userLose(){
-    //     console.log('its a lose');
-    // }
-
-    updateScore(){
-        this.userScore.innerHTML = userPoints.toString();
-        this.computerScore.innerHTML = computerPoints.toString();
-    }
-}
-
 let userPoints = 0;
 let computerPoints = 0;
-const userScore = document.querySelector('user-score');
-const computerScore = document.querySelector('computer-score');
-// const scoreBoard = document.querySelector('.score-board');
+const userScore = document.getElementById('user-score');
+const computerScore = document.getElementById('computer-score');
+// const userHand = document.getElementById('user-hand');
+// const computerHand = document.getElementById('computer-hand');
 const actionText = document.getElementById('action-text');
 const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
 
-const game = new Game(userScore, computerScore);
+function RandomChoice(){
+    let choices = ['rock', 'paper', 'scissors'];
+    let randomNumber = Math.floor(Math.random()*3);
+    return choices[randomNumber];
+}
+
+function convertWord(choice){
+    if(choice === 'rock') return 'Rock';
+    if (choice === 'paper') return 'Paper';
+    return 'Scissors';
+}
+
+function userPlays(userChoice){
+    let computerChoice = RandomChoice();
+    switch(userChoice+computerChoice){
+        case 'rockrock':
+        case 'paperpaper':
+        case 'scissorsscissors':
+            document.getElementById('user-hand').src=`./assets/${userChoice}-hand.png`
+            userTie();
+            break;
+        case 'rockscissors':
+        case 'paperrock':
+        case 'scissorspaper':
+            userWin(userChoice, computerChoice);
+            break;
+        case 'rockpaper':
+        case 'paperscissors':
+        case 'scissorsrock':
+            userLose(userChoice, computerChoice);
+            break;
+    }
+}
+
+function userTie(){
+    actionText.innerHTML = `It's a tie.`
+}
+
+function userWin(userChoice, computerChoice){
+    userPoints++;
+    this.updateScore();
+    actionText.innerHTML = `${convertWord(userChoice)} Beats ${convertWord(computerChoice)}. You Win!`
+}
+
+function userLose(userChoice, computerChoice){
+    computerPoints++;
+    this.updateScore();
+    actionText.innerHTML = `${convertWord(computerChoice)} Beats ${convertWord(userChoice)}. You Lose...`
+}
+
+function updateScore(){
+    userScore.innerHTML = userPoints;
+    computerScore.innerHTML = computerPoints;
+}
 
 rock.addEventListener('click', button => {
-    game.userPlays('rock');
+    userPlays('rock');
 })
 
 paper.addEventListener('click', button => {
-    game.userPlays('paper');
+    userPlays('paper');
 })
 
 scissors.addEventListener('click', button => {
-    game.userPlays('scissors');
+    userPlays('scissors');
 })
